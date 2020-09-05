e8b865e1d43b08447f6774072aa1a5fef08c0ee3
TheodoreChu <22967798+TheodoreChu@users.noreply.github.com>
2020-09-05 14:32:00 -0700

    fix broken links

--- src/pages/index-copy.js
+++ src/pages/index-copy.js
@@ -72,7 +72,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted
               )}
-              to={useBaseUrl('docs/doc1')}
+              to={useBaseUrl('doc1')}
             >
               Get Started
             </Link>
